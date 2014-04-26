790c59e0549c4f3dfebff8f608c9302ebbcc6f42
Andy Young <andymyoung@gmail.com>
2014-04-26 11:51:31 -0700

    Add RSpec formatting options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,14 @@ end
 
 # For RSpec 2.x
 RSpec.configure { |c| c.include RSpecMixin }
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
